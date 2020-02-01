4a87fbb4d6e65543160e5c637011a723cdf2d491
yhusr <904239064@qq.com>
2020-02-01 16:23:21 +0800

    优化版本

--- scripts/handle_re.py
+++ scripts/handle_re.py
@@ -3,7 +3,7 @@ Time:2020/1/31 0031
 """
 import re
 from scripts.handle_mysql import HandleMysql
-from scripts.handle_conf import HandleYaml
+from scripts.handle_conf import HandleYaml, hy
 from scripts.handle_os import PHONE_PATH
 
 
@@ -11,8 +11,9 @@ class HandleRe:
     @classmethod
     def handle_para(cls,data):
         hl = HandleYaml(PHONE_PATH)
+        hm = HandleMysql()
         if re.search(r'{no_exist_phone}', data):
-            phone = HandleMysql().no_exsit_phone_mysql()
+            phone = hm.no_exsit_phone_mysql()
             re_data = re.sub(r'{no_exist_phone}', phone, data)
             HandleMysql().close()
             return re_data
@@ -21,5 +22,14 @@ class HandleRe:
             return re_data
         if re.search(r'{user_id_re}', data):
             re_data = re.sub(r'{user_id_re}', str(hl.read_yaml('investor', 'id')), data)
+            return re_data
+        if re.search(r'{member_id_re}', data):
+            re_data = re.sub(r'{member_id_re}', str(hl.read_yaml('borrower', 'id')), data)
+            return re_data
+        if re.search(r'{borrow_phone}', data):
+            re_data = re.sub(r'{borrow_phone}', hl.read_yaml('borrower', 'mobile_phone'), data)
+            return re_data
+        if re.search(r'{no_exist_num}', data):
+            re_data = re.sub(r'{no_exist_num}', str(hm.get_mysql_result(hy.read_yaml('mysql', 'id_sql'))['maxId']+1), data)
             return re_data
         return data
