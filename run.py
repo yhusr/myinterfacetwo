36cfc7941988c0852f6171961cafd78908faf303
yhusr <41948585+yhusr@users.noreply.github.com>
2021-04-06 00:47:33 +0800

    Update run.py

--- run.py
+++ run.py
@@ -4,5 +4,5 @@ Time:2020/1/31 0031
 import pytest
 
 if __name__ == '__main__':
-    pytest.main(["--reruns", "2", "--reruns-delay", "5",
+    pytest.main(["--reruns", "1", "--reruns-delay", "5",
                  "--junitxml=reports/allure.xml"])
